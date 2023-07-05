d0eff0f08e0741b5fdf92d75ecf4371914316c98
Fernanda Vives <mfernandavives@gmail.com>
2023-07-05 19:16:31 -0400

    Tarea Hacer_conexciones

---
+++ script.js
@@ -0,0 +1,9 @@
+function cambiarTexto() {
+    var parrafo = document.getElementById("cambiarUsuario");
+    parrafo.textContent = "Fernanda Vives";
+}
+
+function eliminarObjeto() {
+    var objeto = document.getElementById("icon1");
+    objeto.remove();
+}
